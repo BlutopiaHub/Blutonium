655f0c499188e71eb8c4b585f8de12c2f942735d
Israel Aristide <44146685+drizzylga1151@users.noreply.github.com>
2020-07-16 13:23:06 -0400

    clean up and update 1.15

--- extensions/events.py
+++ extensions/events.py
@@ -2,32 +2,10 @@ import discord, datetime, time
 from discord.ext import commands
 import asyncio, pytz
 from itertools import cycle
-import MySQLdb
+import blutapi
 from Setup import *
 
 start_time = time.time()
-
-def get_prefix(guild):
-    db = MySQLdb.connect(host=sqhost, user=squname, passwd=sqpassword, db=sqdbname)
-    cur = db.cursor()
-
-    guildid = str(guild.id)
-    sql = f"SELECT * FROM prefixes WHERE id={guildid}"  
-    cur.execute(sql)
-    prefix = cur.fetchone()[1]
-    db.close()
-
-    return prefix
-
-def set_prefix(guild):
-    db = MySQLdb.connect(host=sqhost, user=squname, passwd=sqpassword, db=sqdbname)
-    cur = db.cursor()
-
-    guildid = str(guild.id)
-    sql = f"INSERT INTO prefixes VALUES ({guildid}, '{defprefix}')"
-    cur.execute(sql)
-    db.commit()
-    db.close()
 
 class events(commands.Cog,name='Events'):
     """
@@ -36,22 +14,6 @@ class events(commands.Cog,name='Events'):
     def __init__(self,client):
         
         self.client = client
-
-   # @commands.Cog.listener()
-   # async def on_message(self,msg):
-#
-   #     messagec : str = msg.content
-#
- #       if msg.guild is None:
-  #          return print(f'{msg.author}-{messagec}')
-
-   #     if messagec.startswith('<@'):
-
-    #        prefix = get_prefix(msg.guild)
-
-    #        if self.client.user in msg.mentions:
-      #          await msg.add_reaction('❤️')
-     #           return await msg.channel.send(f'Hi! My prefix for this server is ``{prefix}``')
    
     @commands.command(help='Shows bot uptime')
     async def uptime(self, msg):
@@ -66,45 +28,31 @@ class events(commands.Cog,name='Events'):
         except discord.HTTPException:
             await msg.channel.send(f'⏳ Ive Been up for: `{text}`')
 
-
     @commands.Cog.listener()
     async def on_guild_join(self,guild):
-        set_prefix(guild)
+        prefix = blutapi.getprefix(guild)
 
     @commands.Cog.listener()
     async def on_ready(self):
 
-        print('\nDrizzi is Ready!')
-        print(f'logged in as {self.client.user}\n')
 
-        x = []
+        for guild in self.client.guilds:
+            bruh = blutapi.getprefix(guild)
 
-        for guilds in self.client.guilds:
-            x.append(guilds)
+        statuses = [f'over {len(set(self.client.get_all_members()))} users!', f'over {len(self.client.guilds)} guilds!','Forgot your prefix? @mention me!', 'over your mind']
 
 
-        for guild in x:
-
-            bruh = get_prefix(guild)
-
-            if bruh is None:
-                set_prefix(guild)
-                print(f'[CONSOLE] Sucsessfully set prefix in {guild.name}')
-                
-            else:
-                print(f"""[CONSOLE] Guild '{guild.name}' has a prefix: {bruh}""")
-
-        statuses = [f'over {len(set(self.client.get_all_members()))} users!', f'over {len(self.client.guilds)} guilds!','Forgot your prefix? @metion me!', 'over your mind']
-
-        msgs = cycle(statuses)
+        displaying = cycle(statuses)
 
         running = True
 
+        print('\nBlutonium is operational!')
+        print(f'logged in as {self.client.user}\n')
+
         while running:
-            current_status = next(msgs)
+            current_status = next(displaying)
             await self.client.change_presence(status=discord.Status.online, activity=discord.Activity(name=current_status ,type=3))
             await asyncio.sleep(20)
 
-    
 def setup(client):
     client.add_cog(events(client))
